ca905d9599ad1ddb77be16ce9152dd6851549041
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-09-21 11:29:30 -0700

    git push

---
+++ nop_m.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - opcode nop doesn’t do anything.
+ * @stack: the stack or queue
+ * @line_number: the current line number
+ * Project by Stanley Ibeneme
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+}
