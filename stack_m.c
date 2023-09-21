049a1713ce573c72885a8ef3abcccf744f086cb5
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-09-21 12:10:27 -0700

    stack

---
+++ stack_m.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * stack - it changes to LIFO
+ * @stack: the monty queue stack
+ * @line_number: line of file
+ * project by Stanley Ibeneme
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+	SQ = 1;
+}
+/**
+ * queue - it changes to FIFO
+ * @stack: the monty queu or stack
+ * @line_number: line of file
+ * project by Stanley Ibeneme
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+	SQ = 0;
+}
